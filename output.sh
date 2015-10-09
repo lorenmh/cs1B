b81447c9784548df374b48299498e8742aeffa55
Loren <neutronstar@gmail.com>
2015-10-08 21:36:12 -0700

    assignment 1

--- output.sh
+++ output.sh
@@ -1,4 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
+shopt -s extglob
 cd "${0%/*}"
-APATH=assignment${1}/ ./scripts/make_and_run.sh
+
+ASSIGNMENT=assignment${1}
+
+OUTFILE=completed/$ASSIGNMENT.txt
+
+FILES=$(ls $ASSIGNMENT/*.java | grep -v 'Main.java')
+
+printf "******************************************\n" > $OUTFILE
+echo $ASSIGNMENT/Main.java >> $OUTFILE
+printf "******************************************\n" >> $OUTFILE
+cat $ASSIGNMENT/Main.java >> $OUTFILE
+
+for file in $FILES
+do
+  printf "\n\n******************************************\n" >> $OUTFILE
+  echo $file >> $OUTFILE
+  printf "******************************************\n" >> $OUTFILE
+  cat $file >> $OUTFILE
+done
+
+printf "\n\n******************************************\nCONSOLE OUTPUT" >> $OUTFILE
+printf "\n******************************************\n" >> $OUTFILE
+
+./exec.sh $1 >> $OUTFILE
